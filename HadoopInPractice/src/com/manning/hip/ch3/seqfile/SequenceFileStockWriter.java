5c997711359ccd2a79c44aea05deb990d1d27125
littlestudy <androidstudy2@163.com>
2014-08-17 22:19:24 +0800

    123

---
+++ HadoopInPractice/src/com/manning/hip/ch3/seqfile/SequenceFileStockWriter.java
@@ -0,0 +1,43 @@
+package com.manning.hip.ch3.seqfile;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.DefaultCodec;
+
+import com.manning.hip.ch3.StockPriceWritable;
+
+public class SequenceFileStockWriter {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void write(File inputFile, Path outputPath) throws IOException{
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		
+		SequenceFile.Writer writer = SequenceFile.createWriter(fs, conf
+				, outputPath, Text.class, StockPriceWritable.class
+				, SequenceFile.CompressionType.BLOCK
+				, new DefaultCodec());
+		
+		try {
+			Text key = new Text();
+			
+			for (String line: FileUtils.readLines(inputFile)){
+				StockPriceWritable stock = StockPriceWritable.fromLine(line);
+				key.set(stock.getSymbol());
+				key.set(stock.getSymbol());
+				writer.append(key, stock);
+			}
+		} finally {
+			writer.close();
+		}
+	}
+}
